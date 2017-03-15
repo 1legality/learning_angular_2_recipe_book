c6c57c68b16e964ef828752926eef46467b3c600
1legality <markis.landry@gmail.com>
2017-03-15 18:42:43 -0400

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe";
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html',
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  
+  // Create a dummy receipe
+  recipe = new Recipe('Dummy', 'Dummy', 'https://rlv.zcache.com/crash_test_dummy_marker_classic_round_sticker-r8b36e2d5c0f74a4c843565523094b867_v9wth_8byvr_324.jpg')
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
