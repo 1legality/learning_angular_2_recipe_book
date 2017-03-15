6791bccf7bec6bc3abdb4fb978af1a45b5a7f9fb
1legality <markis.landry@gmail.com>
2017-03-15 19:46:43 -0400

    add recipe detail view and shopping list form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe";
 
 @Component({
@@ -7,7 +7,8 @@ import { Recipe } from "../recipe";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  
+  // Observer emitter for recipe detail
+  @Output() recipeSelected = new EventEmitter();
   // Create a dummy receipe
   recipe = new Recipe('Dummy', 'Dummy', 'https://rlv.zcache.com/crash_test_dummy_marker_classic_round_sticker-r8b36e2d5c0f74a4c843565523094b867_v9wth_8byvr_324.jpg')
 
@@ -16,4 +17,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
