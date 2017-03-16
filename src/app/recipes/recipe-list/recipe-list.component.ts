5d3dd57c951a0619e64d76beb681974d7383ec27
1legality <markis.landry@gmail.com>
2017-03-15 20:51:27 -0400

    add services for ingredients and recipe Also make "To Shopping List" button works

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
-    new Recipe('Summer salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
-  ];
+  recipes: Recipe[] = [];
+
   // Observer emitter for recipe detail
   @Output() recipeSelected = new EventEmitter();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe: Recipe) {
